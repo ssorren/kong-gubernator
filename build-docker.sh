03370cf7ead1fd1f297b7d4dda25e501915a3999
Sal Sorrentino <salvatore.sorrentino@konghq.com>
2025-07-25 11:55:42 -0500

    updating docker build script

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,4 @@
 docker buildx build --platform linux/amd64,linux/arm64 -t kong-gubernated:1.0.0 . 
-docker buildx build --platform linux/amd64,linux/arm64 -t sal1103/kong-gubernated:1.0.0 . 
-docker buildx build --platform linux/amd64,linux/arm64 -t sal1103/kong-gubernated:latest . 
+docker tag kong-gubernated:1.0.0 sal1103/kong-gubernated:latest
 docker push sal1103/kong-gubernated:1.0.0
 docker push sal1103/kong-gubernated:latest
